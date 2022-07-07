d3d672b49440e33e7b849ecc7f85a30d8fe7e334
usananth <us.ananthakumar@gmail.com>
2022-07-07 11:49:59 +0530

    Second Commit

--- restAPIProject/src/test/java/restAPIProject/test.java
+++ restAPIProject/src/test/java/restAPIProject/test.java
@@ -3,6 +3,11 @@ package restAPIProject;
 public class test{
 	public static void main(String arg[]) {
 		System.out.println("The Test is Passed");
+		for(int i=1;i<=108;i++)
+		{
+			System.out.println("OM NAMA SHIVAYA OM");
+		}
+
 	}
 
 }
